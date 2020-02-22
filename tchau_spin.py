2a7f275a2fd9ea4fd785d2b29d83a3a1be3394d1
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2020-02-22 17:30:56 -0500

    fock improved

--- tchau_spin.py
+++ tchau_spin.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-# Index objects
-
 class Index:
+
+    # Class to represente indexes using in quantum chemistry calculations
+    # Must have a name (e.g. i,j,k,l,m,n, etc)
+    # Msut have a spin (alpha, beta or any)
     
     def __init__(self, name, spin='any'):
 
         if len(name) > 1:
             raise NameError('Index must be identified by one letter')
-        self.name == name
-        self.spin == spin
+        self.name = name
+        self.spin = spin
         if spin == 'alpha':
             self.s = 1
         elif spin == 'beta':
@@ -19,7 +21,12 @@ class Index:
         else:
             raise NameError('Invalid Spin Type. Must be alpha, beta or any')
 
-    def __str__(self)
+    def __str__(self):
+
+        # String representation of the index:
+        # Alpha indexes are capitalized
+        # Beta indexes are lower cases
+        # Spin indefined index are represented with '~' before it
 
         if self.s == 1:
             return self.name.upper()
@@ -28,62 +35,106 @@ class Index:
         else:
             return '~'+self.name
 
-    def flip(self)
+    def change_spin(self, s):
 
-        # Flip the spin
+        # Return a copy of the index with the desired spin (0 for beta, 1 for alpha)
 
-        if self.s == 1:
+        if s == 1:
+            return Index(self.name, spin='alpha')
 
-            return Index(name=name, spin = 'alpha')
-
-        if self.s == 1:
-
-            return Index(name=name, spin = 'beta')
+        elif s == 0:
+            return Index(self.name, spin='beta')
 
         else:
-            raise NameError('Cannot flip undefined spin')
-
+            raise ValueError('Invalid spin input. Must be 0 (for beta) or 1 (for alpha)')
 
 class collection:
 
-    # Collection of terms
+    # Object to represent a sum of terms and contractions
 
-    def __init__(self):
+    def __init__(self, terms=[]):
 
-        self.terms = []
+        self.terms = terms
+
+    # Define addition operations
 
     def __add__(self, other):
 
-        self.terms = []
+        if other == 0:
+            return self
 
-    def __str__(self, other):
+        elif type(other) == fock_rhf:
+
+            newterms = self.terms[:]
+            newterms.append(other)
+            return collection(newterms)
+
+        elif type(other) == collection:
+            newterms = self.terms[:] + other.terms[:]
+            return collection(newterms)
+
+        else:
+            return TypeError('Addition undefined for collection and {}'.format(type(other)))
+
+    def __radd__(self, other):
+
+        return self + other
+
+    def __str__(self):
 
         out  = ''
-        for x in terms:
-            out += str(x)
+        for x in self.terms:
+            out += str(x) + '   '
 
-        print(out)
+        return out
 
 
 class fock_rhf:
 
     # Object that represents a fock matrix for restricted orbitals
 
-    def __init__(self, index_1, index_2):
+    def __init__(self, index_1, index_2, prefac=1):
 
         self.idx = [index_1, index_2]
-        self.prefac = 1.0
+        self.prefac = prefac
+
+
+    def __add__(self, other):
+
+        # Addition: first put self into a collection, then call collectin addition method
+        
+        c = collection() + self
+
+        return c + other
+
+    def __mul__(self, other):
+
+        if type(other) == float or type(other) == int:
+            
+            return fock_rhf(self.idx[0], self.idx[1], prefac = self.prefac*other)
+
+        else:
+
+            raise TypeError('Addition undefined for fock and {}'.format(type(other)))
+
+    def __rmul__(self, other):
+
+        return self*other
 
     def __str__(self):
 
-        if self.prefac != 1:
+        if self.prefac == -1:
+            return '-f(' + str(self.idx[0]) + ',' + str(self.idx[1]) + ')'
 
-            return str(self.prefac) +'*f(' + str(self.i1) + ',' + str(self.i2) + ')'
+        elif self.prefac == 1:
+            return '+f(' + str(self.idx[0]) + ',' + str(self.idx[1]) + ')'
 
-        return 'f(' + str(self.i1) + ',' + str(self.i2) + ')'
+        return str(self.prefac) +'*f(' + str(self.idx[0]) + ',' + str(self.idx[1]) + ')'
 
-    def spin_cases(self):
+    def expand_spin(self):
 
+        # Array with all possible combinations os alpha and beta for each index
+        # One represent an allowed spin case
         possible = np.ones((2,2))
         
         # For fock matrix f(p,q) spin of p has to be equal spin q. Thus
@@ -94,16 +145,29 @@ class fock_rhf:
                     possible[p,q] = 0
 
         # Check if any index has spin defined:
-        if self.i1 == 0:
+        if self.idx[0].spin == 'beta':
             possible[1,:] = 0
 
-        if self.i1 == 1:
+        if self.idx[0].spin == 'alpha':
             possible[0,:] = 0
 
+        if self.idx[1].spin == 'beta':
+            possible[:,1] = 0
+
+        if self.idx[1].spin == 'alpha':
+            possible[:,0] = 0
+
         out = collection()
+        
+        for p in range(2):
+            for q in range(2):
+                if possible[p,q] == 1:
+                    ip = self.idx[0].change_spin(p)
+                    iq = self.idx[1].change_spin(q)
+                    out += fock_rhf(ip, iq)
 
-            
-
+        return out
+                    
     def spin_simplify(self):
 
         if any([x.s == 2 for x in self.idx]):
@@ -114,10 +178,16 @@ class fock_rhf:
 
             pass
 
+class ERI:
+
+    def __init__(p, q, r, s, prefac):
+
+        self.idx = [p,q,r,s]
+        self.prefac = 1.0
                 
-class Amplitude:
-
-    def __init__(self, name, 
-
+#class Amplitude:
+#
+#    def __init__(self, name, 
+#
 
 
