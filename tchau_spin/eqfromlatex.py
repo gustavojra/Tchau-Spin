d147e1e3a689b11793ced2e599f744831d3d24f6
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2020-05-11 18:33:56 -0400

    Eq from latex now can understand permutation operators

--- tchau_spin/eqfromlatex.py
+++ tchau_spin/eqfromlatex.py
@@ -10,13 +10,44 @@ tensor_keys = {
     'T' : Amplitude
 }
 
+# Given a permutation string (e.g. P(i,j)[X_{i,j}] = X_{i,j} - X_{j,i})
+
+def expand_permutation(string, printv):
+    m = re.match(r"P\((\w+?)/(\w+?)\)\[(.+?)\]", string)
+    printv('Permutation found: {}'.format(m.group(0)))
+    y = ''
+    I1 = m.group(1)
+    I2 = m.group(2)
+    s  = m.group(3)
+    y += s
+    if s[0] not in '+-':
+        s = '+' + s
+    
+    # Change sign of s
+    s = re.sub('[+]', 'PLUS', s)
+    s = re.sub('[-]', '+', s)
+    s = re.sub('PLUS', '-', s)
+    
+    for i in I1:
+        for j in I2:
+            printv('\nPermuting {} and {}'.format(i,j))
+            hold = re.sub(i, 'HOLD', s)
+            hold = re.sub(j, i, hold)
+            hold = re.sub('HOLD', j, hold)
+            printv(hold)
+            y += ' ' + hold
+
+    return y
+
 def eqfromlatex(inp, index_keys, verbose=False, antisymmetric=True):
+
+    printv = print if verbose else lambda *k,**w: None
 
     # Generate a Collection object with a latex equation given in the
     # string 'inp' 
 
-    if verbose: print('Cleaning up equation...')
-    # Remove spaces betweem signs and next terms
+    printv('Cleaning up equation...')
+    # Remove spaces between signs and tensors
     out = re.sub('([+-])\s', '\\1', inp)
     # Add * between tensors contraction
     out = re.sub('}\s+?(\w)', '}*\\1', out)
@@ -32,7 +63,30 @@ def eqfromlatex(inp, index_keys, verbose=False, antisymmetric=True):
     # Remove fractions
     out = re.sub('\\\\frac{(.+?)}{(\d+?)}', '(1/\\2)*\\1', out)
 
-    if verbose: print('Equation after clean up:')
-    if verbose: print(out)
+    # Expand permutations
+    
+    hold = ''
+    for term in out.split():
+        perms = list(re.findall(r"P\(\w+?/\w+?\)", term))
+        if len(perms) == 0:
+            hold += term + ' ' 
+            continue
+        permuting = term
+        for p in perms:
+            permuting = permuting.replace(p, '')
+        
+        permuting = permuting.replace('[', '')
+        permuting = permuting.replace(']', '')
+
+        for p in perms[::-1]:
+            s = p + '[' + permuting + ']'
+            expanded = expand_permutation(s, printv)
+            permuting = expanded
+            printv('\nFinal Expansion on {}:\n'.format(p), expanded)
+
+        hold += permuting + ' '
+
+    out = hold[:-1]
+
 
     return eqfromstring(out, tensor_keys, index_keys, verbose=verbose)
